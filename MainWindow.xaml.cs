dc99c5ec3f77b82085f5cd0f1d95f69af14b9549
Rhubarb <76910193+RhubarbDev@users.noreply.github.com>
2023-02-19 21:48:34 +0000

    invisible buttons + no rectangle clicking

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace TowerOfHanoi
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
     public partial class MainWindow : Window
     {
         const int blocks = 8;
@@ -32,35 +26,28 @@ namespace TowerOfHanoi
 
         }
 
-        void rectangle_MouseLeftDown(object sender, MouseButtonEventArgs e)
-        {
-            if (!selected)
-            {
-                Canvas parent = VisualTreeHelper.GetParent(sender as DependencyObject) as Canvas;
-                if (parent.Children[parent.Children.Count - 1] == sender)
-                {
-                    
-                    selected = true;
-                    parent.Children.Remove(sender as Rectangle);
-                    Rectangle rect = sender as Rectangle;
-                    Canvas.SetTop(rect, 0);
-                    Select.Children.Add(rect);
-                    ToggleButtons();
-                }
-            }
-        }
         private void Button_Click(object sender, RoutedEventArgs e)
-        { 
+        {
             Canvas canvas = sender == RButton ? Right : (sender == MButton ? Middle : Left);
-            Rectangle rect = Select.Children[0] as Rectangle;
-            if(canvas.Children.Count == 0 || (canvas.Children[canvas.Children.Count - 1] as Rectangle).Width > rect.Width)
+            if (!selected && canvas.Children.Count > 0)
             {
-                Select.Children.RemoveAt(0);
-                Canvas.SetTop(rect, (blocks - canvas.Children.Count) * 25);
-                canvas.Children.Add(rect);
-                ToggleButtons();
-                selected = false;
-                CheckWin();
+                selected = true;
+                Rectangle rect = canvas.Children[canvas.Children.Count - 1] as Rectangle;
+                canvas.Children.Remove(rect);
+                Canvas.SetTop(rect, 0);
+                Select.Children.Add(rect);
+            }
+            else if (selected)
+            {
+                Rectangle rect = Select.Children[0] as Rectangle;
+                if (canvas.Children.Count == 0 || (canvas.Children[canvas.Children.Count - 1] as Rectangle).Width > rect.Width)
+                {
+                    Select.Children.RemoveAt(0);
+                    Canvas.SetTop(rect, (blocks - canvas.Children.Count) * 25);
+                    canvas.Children.Add(rect);
+                    selected = false;
+                    CheckWin();
+                }
             }
         }
 
@@ -79,22 +66,14 @@ namespace TowerOfHanoi
             }
         }
 
-        private void ToggleButtons()
-        {
-            LButton.IsEnabled ^= true;
-            MButton.IsEnabled ^= true;
-            RButton.IsEnabled ^= true;
-        }
-
         private void AddBlock(int size, Canvas canvas)
         {
             Rectangle rect = new Rectangle
             {
                 Width = size * 20,
                 Height = 25,
-                Fill = new SolidColorBrush(Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256)))
+                Fill = new SolidColorBrush(Color.FromRgb((byte)rand.Next(255), (byte)rand.Next(255), (byte)rand.Next(255)))
             };
-            rect.MouseLeftButtonDown += rectangle_MouseLeftDown;
             Canvas.SetLeft(rect, ((blocks - size) * 10) + RectangleMargin);
             Canvas.SetTop(rect, size * 25);
             canvas.Children.Add(rect);
